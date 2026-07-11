d46a814ebe569c1f71922ce413488bf80d244319
sujalkadam2906 <sujalkadam1032@gmail.com>
2026-07-11 21:55:54 +0530

    Time: 0 ms (100%), Space: 48.3 MB (11.46%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,18 +1,17 @@
 class Solution {
     public double myPow(double x, int n) {
         long exp = n;
-        if (exp < 0){
+        if(exp < 0){
             x = 1/x;
             exp = -exp;
         }
-        double ans = 1;
-        while ( exp > 0 ){
-            if ( exp % 2 != 0){
-                ans *= x;
-            }
-            x *= x;
-            exp/=2;
+        return power ( x ,  exp , 1);
+    }
+    double power (double x, long n, double ans){
+        if (n == 0) return ans;
+        if (n % 2 != 0) {
+        ans *= x;
         }
-        return ans;
+        return power (x*x , n/2, ans);
     }
 }
