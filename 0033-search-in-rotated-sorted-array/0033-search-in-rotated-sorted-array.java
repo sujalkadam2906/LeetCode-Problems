d9ab6a599b477fbfa34798f7a78eb9ab2d1b638a
sujal2906 <sujalkadam1032@gmail.com>
2026-07-29 09:03:50 +0530

    Time: 0 ms (100%), Space: 44 MB (11.2%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,35 +6,32 @@ class Solution {
 
         while (left <= right) {
 
-            // Middle element nikaalo
             int mid = left + (right - left) / 2;
 
             // Target mil gaya
             if (nums[mid] == target)
                 return mid;
 
-            // Check karo ki left half sorted hai ya nahi
+            // Left half sorted hai
             if (nums[left] <= nums[mid]) {
 
-                // Agar target left sorted range ke andar hai
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;      // Left half me search karo
-                } else {
-                    left = mid + 1;       // Right half me search karo
-                }
+                if (target >= nums[left] && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
 
-            } else { // Right half sorted hai
+            }
+            // Right half sorted hai
+            else {
 
-                // Agar target right sorted range ke andar hai
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;       // Right half me search karo
-                } else {
-                    right = mid - 1;      // Left half me search karo
-                }
+                if (target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+
             }
         }
 
-        // Target nahi mila
         return -1;
     }
 }
