07f5a39b4c19e6b6dfbe9d05f86e6d4a7e53ca7c
sujal2906 <sujalkadam1032@gmail.com>
2026-08-02 11:58:26 +0530

    Time: 0 ms (100%), Space: 43.4 MB (76.98%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+
+        // Stores the rank of every alien character
+        int[] rank = new int[26];
+
+        // Build the mapping
+        // Example:
+        // h -> 0
+        // l -> 1
+        // a -> 2
+    
+        for (int i = 0; i < order.length(); i++) {
+            rank[order.charAt(i) - 'a'] = i;
+        }
+
+        // Compare every adjacent pair of words
+        for (int i = 0; i < words.length - 1; i++) {
+
+            String first = words[i];
+            String second = words[i + 1];
+
+            int j = 0;
+
+            // Compare character by character
+            while (j < first.length() && j < second.length()) {
+
+                char c1 = first.charAt(j);
+                char c2 = second.charAt(j);
+
+                // Characters are different
+                if (c1 != c2) {
+
+                    // If first character comes after second
+                    // in alien order, words are not sorted
+                    if (rank[c1 - 'a'] > rank[c2 - 'a'])
+                        return false;
+
+                    // Correct order
+                    break;
+                }
+
+                j++;
+            }
+
+            // Prefix case
+            // Example:
+            // "apple"
+            // "app"
+            if (j == second.length() && first.length() > second.length())
+                return false;
+        }
+
+        return true;
+    }
+}
