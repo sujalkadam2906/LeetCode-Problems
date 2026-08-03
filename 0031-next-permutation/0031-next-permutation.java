21688ea0953170f5228cfb7876050d68edbf8093
sujal2906 <sujalkadam1032@gmail.com>
2026-08-03 11:14:12 +0530

    Time: 0 ms (100%), Space: 43.7 MB (99.84%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -3,8 +3,7 @@ class Solution {
 
         int n = nums.length;
 
-        // Step 1:
-        // Find the first decreasing element from the end
+        // Step 1:Find the first decreasing element from the end
         int pivot = -1;
 
         for (int i = n - 2; i >= 0; i--) {
@@ -14,16 +13,13 @@ class Solution {
             }
         }
 
-        // Step 2:
-        // If no pivot is found,
-        // current permutation is the largest
+        // Step 2:If no pivot is found,current permutation is the largest
         if (pivot == -1) {
             reverse(nums, 0, n - 1);
             return;
         }
 
-        // Step 3:
-        // Find the first element greater than pivot
+        // Step 3:Find the first element greater than pivot
         for (int i = n - 1; i > pivot; i--) {
 
             if (nums[i] > nums[pivot]) {
@@ -33,13 +29,12 @@ class Solution {
             }
         }
 
-        // Step 4:
-        // Reverse the suffix
+        // Step 4:Reverse the suffix
         reverse(nums, pivot + 1, n - 1);
     }
 
     // Swap two elements
-    private void swap(int[] nums, int i, int j) {
+    void swap(int[] nums, int i, int j) {
 
         int temp = nums[i];
         nums[i] = nums[j];
@@ -47,7 +42,7 @@ class Solution {
     }
 
     // Reverse a part of the array
-    private void reverse(int[] nums, int left, int right) {
+    void reverse(int[] nums, int left, int right) {
 
         while (left < right) {
 
