994df22fd4a63f3a29365aeb4ccfaac882fa279f
sujal2906 <sujalkadam1032@gmail.com>
2026-08-10 11:09:54 +0530

    Time: 6 ms (43.02%), Space: 44.1 MB (95.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,40 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        if (s.length() != t.length()) return false;
 
-        int [] count = new int [26];
+        // Anagrams must have the same length
+        if (s.length() != t.length()) {
+            return false;
+        }
 
-        for (int i = 0; i < s.length(); i++){
+        /*
+         * Frequency array for 26 lowercase English letters.
+         *
+         * index 0  -> 'a'
+         * index 1  -> 'b'
+         * ...
+         * index 25 -> 'z'
+         */
+        int[] count = new int[26];
+
+        // Count characters from s
+        // and remove characters from t
+        for (int i = 0; i < s.length(); i++) {
+
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
-        for (int i = 0; i<26; i++){
-            if(count[i] != 0) return false;
+
+        /*
+         * If s and t are anagrams,
+         * every frequency should become 0.
+         */
+        for (int frequency : count) {
+
+            if (frequency != 0) {
+                return false;
+            }
         }
+
         return true;
     }
 }
