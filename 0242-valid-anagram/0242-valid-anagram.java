943a3744ceb0317763405338bbce53bc89234d68
sujal2906 <sujalkadam1032@gmail.com>
2026-08-10 11:10:42 +0530

    Time: 5 ms (61.61%), Space: 44.2 MB (90.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,29 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        if (s.length() != t.length()) return false;
 
-        int [] count = new int [26];
+        // Anagram strings must have the same length
+        if (s.length() != t.length()) {
+            return false;
+        }
 
-        for (int i = 0; i < s.length(); i++){
+        // Frequency array for 26 lowercase English letters
+        int[] count = new int[26];
+
+        // Count characters in s
+        // and subtract characters in t
+        for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
-        for (int i = 0; i<26; i++){
-            if(count[i] != 0) return false;
+
+        // If any frequency is not zero,
+        // the strings are not anagrams
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
         }
+
         return true;
     }
 }
